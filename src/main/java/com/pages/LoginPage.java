e0d072fc363f0ed31cd19f30c563e68c558c1681
rajendraselenium <rsahoo1977@gmail.com>
2022-11-12 17:14:04 +0530

    my first commit

---
+++ src/main/java/com/pages/LoginPage.java
@@ -0,0 +1,10 @@
+package com.pages;
+
+public class LoginPage {
+
+	
+	public void loginPage()
+	{
+		System.out.println("this is login page objects");
+	}
+}
---
+++ src/test/java/com/testcases/LoginTestCase.java
@@ -0,0 +1,11 @@
+package com.testcases;
+
+public class LoginTestCase {
+
+	
+	
+	public void loginTest()
+	{
+		System.out.println("this is a test case Login Page");
+	}
+}
